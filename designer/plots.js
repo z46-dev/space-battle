f8982bcf752906c6003c10f6735980e8697fa9a4
z46-dev <45317380+z46-dev@users.noreply.github.com>
2023-09-14 06:18:38 -0400

    asd

--- designer/plots.js
+++ designer/plots.js
@@ -1,95 +1,13 @@
-function drawDotPlot(dataSet, step = 1, drawCurveOverData = false) {
-    const min = Math.min(...dataSet);
-    const max = Math.max(...dataSet);
-    const range = max - min;
+// TODO, INCLUDE GAPS INTO THE THINGY DATA SET PARSING
 
-    const canvas = document.createElement("canvas");
-    const ctx = canvas.getContext("2d");
-
-    canvas.width = (range / step) * 45 + 30;
-    canvas.height = 250;
-
-    canvas.style.width = `${canvas.width / window.devicePixelRatio}px`;
-    canvas.style.height = `${canvas.height / window.devicePixelRatio}px`;
-
-    ctx.fillStyle = "#FFFFFF";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-    ctx.strokeStyle = "#000000";
-    ctx.lineWidth = 2;
-
-    ctx.beginPath();
-    ctx.moveTo(0, 225);
-    ctx.lineTo(canvas.width, 225);
-    ctx.stroke();
-    ctx.closePath();
-
-    const highPoints = [];
-
-    for (let i = min; i <= max; i += step) {
-        const x = (i - min) / (range / step / 2 + step) * canvas.width + 15;
-        ctx.beginPath();
-        ctx.moveTo(x, 220);
-        ctx.lineTo(x, 230);
-        ctx.stroke();
-        ctx.closePath();
-
-        ctx.save();
-        ctx.translate(x, 240);
-        ctx.fillStyle = "#000000";
-        ctx.font = "20px sans-serif";
-        ctx.textBaseline = "middle";
-        ctx.textAlign = "center";
-        ctx.fillText(i, 5, 0);
-        ctx.restore();
-
-        const myCount = dataSet.filter(x => x === i).length;
-
-        ctx.save();
-        ctx.translate(x, 210);
-        ctx.fillStyle = "#000000";
-
-        for (let j = 0; j < myCount; j++) {
-            ctx.beginPath();
-            ctx.arc(0, -j * 15, 5, 0, Math.PI * 2);
-            ctx.fill();
-            ctx.closePath();
-        }
-
-        if (myCount > 0) {
-            highPoints.push({
-                x: x,
-                y: 210 - myCount * 15
-            });
-        }
-
-        ctx.restore();
-    }
-
-    if (drawCurveOverData) {
-        ctx.strokeStyle = "#C8C8AA";
-        ctx.beginPath();
-        ctx.moveTo(highPoints[0].x, highPoints[0].y);
-
-        for (let i = 1; i < highPoints.length; i++) {;
-
-            ctx.lineTo(highPoints[i].x, highPoints[i].y);
-        }
-
-        ctx.stroke();
-        ctx.closePath();
-    }
-
-    return canvas;
-}
-
-function drawStemPlot(dataSet, keyInfo = "...") {
+export default function drawSingleStemPlot(dataSet, keyInfo = "...") {
     // Parse data set
     const data = {};
+    const isDecimalSet = dataSet.some(x => x !== x | 0);
 
     for (const value of dataSet.map(x => x.toString())) {
-        if (value.includes(".")) {
-            const [whole, decimal] = value.split(".");
+        if (isDecimalSet) {
+            const [whole, decimal] = (value.includes(".") ? value : (value + ".0")).split(".");
 
             if (!data[whole]) {
                 data[whole] = [];
@@ -107,8 +25,6 @@ function drawStemPlot(dataSet, keyInfo = "...") {
             data[firstPart].push(lastDigit);
         }
     }
-
-    console.log(data);
 
     const canvas = document.createElement("canvas");
     const ctx = canvas.getContext("2d");
@@ -177,7 +93,3 @@ function drawStemPlot(dataSet, keyInfo = "...") {
 
     return canvas;
 }
-
-document.body.appendChild(drawDotPlot([9, 6, 8, 7, 8, 8, 6, 6.5, 7, 7, 9, 4, 3, 4, 5, 6, 11, 6, 3, 7, 6, 10, 7, 8, 4.5, 9, 7, 7], .5, true));
-
-document.body.appendChild(drawStemPlot([17.1, 17.4, 16.6, 17.4, 17.7, 17.1, 17.3, 17.7, 17.8, 19.2, "16.0", 15.9, 16.5, 16.8, 16.5, 17.1, 16.7], "15|9 = 15.9"));
