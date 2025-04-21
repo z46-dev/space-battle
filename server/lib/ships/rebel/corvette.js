a1d938c432c934f13fd0891eba5ec98217bbd240
z46-dev <evanparker6023@gmail.com>
2025-04-21 11:27:26 -0400

    rebel balancing

--- server/lib/ships/rebel/corvette.js
+++ server/lib/ships/rebel/corvette.js
@@ -133,11 +133,11 @@ ships.FORRAYBLOCKADERUNNER_REBEL = {
     asset: "FORRAYBLOCKADERUNNER.png",
     classification: shipTypes.Corvette,
     population: 2,
-    size: 210,
-    cost: 210,
+    size: 100,
+    cost: 310,
     speed: 9.5,
     turnSpeed: .02,
-    shield: 1500,
+    shield: 350,
     shieldRegen: 1.5,
     hardpoints: [{
         x: 0,
@@ -150,12 +150,6 @@ ships.FORRAYBLOCKADERUNNER_REBEL = {
         y: .425,
         weapon: weapons.ION_CANNON,
         shotsAtOnce: 4,
-        shotDelay: 75
-    }, {
-        x: 0,
-        y: 0,
-        weapon: weapons.ASSAULT_PROTON_TORPEDO,
-        shotsAtOnce: 6,
         shotDelay: 75
     }, {
         x: 0,
